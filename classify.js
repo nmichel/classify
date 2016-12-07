b94c2d517ec8f2f16a20d2403905560332464c43
Nicolas Michel <nicolas.michel.lava@gmail.com>
2016-12-07 21:48:22 +0100

    Avoid bloating closure by moving some functions

--- classify.js
+++ classify.js
@@ -1,45 +1,47 @@
 (function() {
+
   var defaultCtor = function() {
     this.super();
   };
 
+
+  var lookup = function(base, name) {
+    var fn = undefined;
+    var p = base.prototype;
+    while (!fn && p) {
+      fn = p[name];
+      p = p.prototype;
+    }
+
+    return fn;
+  };
+    
+  var buildSuper = function(base, name) {
+    var fn = lookup(base, name);
+
+    if (fn === undefined || typeof fn !== 'function') {
+      return function() {
+        throw new Error('no method "' + name + '" found'); // <== 
+      };
+    }
+
+    return fn;
+  };
+
+  var buildWrapper = function(base, what, name) {
+    var super_ = buildSuper(base, name);
+    return function() {
+      var params = [].splice.call(arguments, 0);
+      var oldsuper = this.super;
+      this.super = super_;
+      var r = what.apply(this, params);
+      this.super = oldsuper;
+      return r;
+    }
+  };
+
   function buildClass(desc) {
     var B = desc.extend || Object;
-
-    var lookup = function(name) {
-      var fn = undefined;
-      var p = B.prototype;
-      while (!fn && p) {
-        fn = p[name];
-        p = p.prototype;
-      }
-
-      return fn;
-    };
-
-    var buildSuper = function(name) {
-      var fn = lookup(name);
-
-      if (fn === undefined || typeof fn !== 'function') {
-        return function() {
-          throw new Error('no method "' + name + '" found'); // <== 
-        };
-      }
-
-      return fn;
-    };
-
-    var buildWrapper = function(what, name) {
-      var super_ = buildSuper(name);
-      return function() {
-        var params = [].splice.call(arguments, 0);
-        var oldsuper = this.super;
-        this.super = super_;
-        var r = what.apply(this, params);
-        this.super = oldsuper;
-        return r;
-      }
-    };
 
     var ctor = desc.ctor || defaultCtor;
     var clazz = function() {
@@ -57,7 +59,7 @@
     for (var m in methods) {
       props[m] = {
         enumerable: true,
-        value: buildWrapper(methods[m], m)
+        value: buildWrapper(B, methods[m], m)
       };
     }
     Object.defineProperties(clazz.prototype, props);
@@ -81,5 +83,6 @@
   else if (typeof module !== 'undefined' && module.exports) {
     module.exports = buildClass;
   }
+
 })();
 
